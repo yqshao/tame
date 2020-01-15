9c55b2fcdc5a7842454422565950bcf4eb925016
Yunqi Shao <yunqi.shao@kemi.uu.se>
2020-01-15 14:18:53 +0100

    [cli] remove `required` for python 3.6 compat.

--- mdppp/recipes/bin.py
+++ mdppp/recipes/bin.py
@@ -5,10 +5,10 @@ import argparse
 def main():
     from importlib import import_module
     parser = argparse.ArgumentParser(prog='mdppp')
-    subps = parser.add_subparsers(dest='command', title='subcommands', required=True)
+    subps = parser.add_subparsers(dest='command', title='subcommands')
     subp = subps.add_parser('cond', help='Conductivity Analysess')
     
-    subps = subp.add_subparsers(title='methods', dest='command', required=True)
+    subps = subp.add_subparsers(title='methods', dest='command')
     subp = subps.add_parser('jacf', help='Green-Kubo method '\
                             'with current density autocorrelation function')
     import_module('mdppp.recipes.cond_jacf').set_parser(subp)
