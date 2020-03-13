8dd3d0e33be60dc090ecf597fcae5b22216b0311
Yunqi Shao <yunqi.shao@kemi.uu.se>
2020-03-13 14:10:36 +0100

    [io] handel different lammps formats

--- mdppp/io/lammps.py
+++ mdppp/io/lammps.py
@@ -4,6 +4,14 @@ import numpy as np
 
 def _dump_generator(fname, resort=False):
     f = open(fname)
+    [next(f) for _ in range(8)]
+    l = f.readline()
+    fmt = l.split()[2:]
+    idx_coord = [fmt.index(key) for key in ['x','y','z']]
+    idx_speed = [fmt.index(key) for key in ['vx','vy','vz']]
+    idx_elem = fmt.index('type')
+    f.close()
+    f = open(fname)
     [next(f) for _ in range(3)]
     natoms = int(f.readline())
     def get_cell(line): return float(line.split()[1]) - float(line.split()[0])
@@ -14,14 +22,14 @@ def _dump_generator(fname, resort=False):
             break
         if line.startswith('ITEM: BOX'):
             cell = np.array([get_cell(f.readline()) for i in range(3)])
-            f.readline()
+            next(f)
             count += 1
             coord, speed, elems = [], [], []
             for i in range(natoms):
                 line = f.readline().split()
-                coord.append(line[2:5])
-                speed.append(line[5:8])
-                elems.append(line[1])
+                coord.append([line[idx] for idx in idx_coord])
+                speed.append([line[idx] for idx in idx_speed])
+                elems.append(line[idx_elem])
             elems = np.array(elems, np.int32)
             data = {
                 'coord': np.array(coord, np.float64),
