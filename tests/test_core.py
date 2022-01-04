6e827a2a90f1fcae1e054b477045ca9c379de9c2
Yunqi Shao <yunqi.shao@kemi.uu.se>
2022-01-04 14:34:15 +0100

    [test] fix test_core

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,22 +2,22 @@ import numpy as np
 
 def test_framedata():
     from tame import FrameData
-    from tame.ops import CumMean
+    from tame.ops import tavg
     
     data = np.array([1,2,3,4,5])
-    iterator = iter(data)
-    fdata = FrameData({'a': iterator})
+    iterator = map(lambda x: {'a':x}, iter(data))
+    fdata = FrameData(iterator)
 
     a = fdata['a'] # A FrameArray object
     b = a * 2      # it should be able to do arithmetics
     c = np.sin(a)  # it should accept numpy ufuncs
-    d = CumMean(a) # A FrameOp object
+    d = tavg(a)    # A FrameOp object
     # The above variables should be updated automatically
-    for i in range(5):
+    for i in range(4):
         assert a.eval()==data[i]
         assert b.eval()==data[i]*2
         assert c.eval()==np.sin(data[i])
-        assert d.eval()==np.mean(data[:i])
+        assert d.eval()==np.mean(data[:i+1])
         fdata.next_frame()
 
 
